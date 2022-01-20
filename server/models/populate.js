0a578e7ff057cc77cebb26f9f97db3a85b0579c2
joseortiz09 <jose@bluepixel.mx>
2022-01-20 17:24:11 +0300

    feature populate files

--- server/models/populate.js
+++ server/models/populate.js
@@ -1,9 +1,23 @@
-const {FileStatus, File} = require('./File'),
+const faker = require('@faker-js/faker'),
+    {FileStatus, File} = require('./File'),
     {User} = require('./User'),
     {Hashtag, Category, Utensil, RecipeFile, Measure, Product, Recipe} = require('./Recipe'),
     recipeBook = require('./RecipeBook');
 
 const populateAll = async () => {
+    await FileStatus.create({name: 'Ok'});
+    await FileStatus.create({name: 'Processing'});
+    await FileStatus.create({name: 'Error'});
+    const okStatus = await FileStatus.findOne({where: {name: 'Ok'}});
+    const files = new Array(10).fill({
+        name: faker.image.food(),
+        url: faker.image.food(),
+        size: 100,
+        statusId: okStatus.id,
+    });
+    await File.bulkCreate(files);
+
+
 };
 
 populateAll()
